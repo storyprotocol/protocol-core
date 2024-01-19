09de15aa20a07cfcac87ad714c9e7a36826d07a9
Raul <raul@proofofcode.dev>
2024-01-19 12:01:45 -0800

    WIP

---
+++ contracts/modules/tagging/TaggingModule.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import { ShortString, ShortStrings } from "@openzeppelin/contracts/utils/ShortStrings.sol";
+
+contract TaggingModule {
+    using ShortStrings for *;
+
+    mapping(
+
+    struct Relation {
+        // src tag
+        // dst tag
+    }
+
+    function 
+}
