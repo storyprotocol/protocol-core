4c9d6f4582d2242a824c0ed4084cf53283f82c88
Raul <raul@proofofcode.dev>
2024-01-24 14:10:15 -0300

    stray activation method

--- contracts/interfaces/licensing/IParamVerifier.sol
+++ contracts/interfaces/licensing/IParamVerifier.sol
@@ -6,6 +6,5 @@ interface IParamVerifier {
     function verifyMinting(address caller, uint256 amount, bytes memory data) external returns (bool);
     function verifyTransfer(address caller, uint256 amount, bytes memory data) external returns (bool);
     function verifyLinkParent(address caller, bytes memory data) external returns (bool);
-    function verifyActivation(address caller, bytes memory data) external returns (bool);
     function json() external view returns (string memory);
 }
