7f0575dc08404807a860361c8d3fe054344820a7
Raul <raul@proofofcode.dev>
2024-01-30 16:24:34 -0300

    rename IParamVerifier into IPolicyVerifier

--- contracts/interfaces/licensing/ILinkParamVerifier.sol
+++ contracts/interfaces/licensing/ILinkParamVerifier.sol
@@ -7,11 +7,7 @@ import { IPolicyVerifier } from "contracts/interfaces/licensing/IPolicyVerifier.
 /// @title ILinkParamVerifier
 /// @notice LicenseRegistry will call this to verify the linking an IP to its parent
 /// with the policy referenced by the license in use.
-<<<<<<< HEAD
 interface ILinkParamVerifier is IPolicyVerifier {
-=======
-interface ILinkParamVerifier is IParamVerifier {
->>>>>>> 5b1360b (comments and linting)
     function verifyLink(
         uint256 licenseId,
         address caller,
