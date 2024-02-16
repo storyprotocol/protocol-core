a70a1ececb81c407deec129d11b03f474a280442
Leeren Chang <leeren@leerenchang.com>
2024-02-15 19:08:35 -0800

    Removes the module interface from the resolver interface

--- contracts/interfaces/resolvers/IResolver.sol
+++ contracts/interfaces/resolvers/IResolver.sol
@@ -2,10 +2,8 @@
 // See https://github.com/storyprotocol/protocol-contracts/blob/main/StoryProtocol-AlphaTestingAgreement-17942166.3.pdf
 pragma solidity ^0.8.23;
 
-import { IModule } from "../modules/base/IModule.sol";
-
 /// @notice Resolver Interface
-interface IResolver is IModule {
+interface IResolver {
     /// @notice Checks whether the resolver IP interface is supported.
     function supportsInterface(bytes4 id) external view returns (bool);
 }
