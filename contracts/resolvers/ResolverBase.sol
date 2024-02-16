caa2519660233ba8cb539099453fbbd467c2aa92
Jongwon Park <jwparktom@gmail.com>
2024-02-16 04:36:55 -0600

    fix: reflect main on IResolver change

--- contracts/resolvers/ResolverBase.sol
+++ contracts/resolvers/ResolverBase.sol
@@ -2,8 +2,6 @@
 // See https://github.com/storyprotocol/protocol-contracts/blob/main/StoryProtocol-AlphaTestingAgreement-17942166.3.pdf
 pragma solidity ^0.8.23;
 
-import { IERC165 } from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
-
 import { BaseModule } from "../modules/BaseModule.sol";
 import { IResolver } from "../interfaces/resolvers/IResolver.sol";
 import { AccessControlled } from "../access/AccessControlled.sol";
@@ -13,7 +11,7 @@ abstract contract ResolverBase is IResolver, BaseModule, AccessControlled {
     constructor(address accessController, address assetRegistry) AccessControlled(accessController, assetRegistry) {}
 
     /// @notice IERC165 interface support.
-    function supportsInterface(bytes4 id) public view virtual override(BaseModule, IERC165) returns (bool) {
+    function supportsInterface(bytes4 id) public view virtual override(BaseModule, IResolver) returns (bool) {
         return id == type(IResolver).interfaceId || super.supportsInterface(id);
     }
 }
